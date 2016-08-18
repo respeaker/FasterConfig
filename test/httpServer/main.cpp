99c8ef8765894477391bb589cc3ca9c721bfc18c
Baozhu Zuo <zuobaozhu@gmail.com>
2016-08-18 21:29:47 +0800

    test http class

--- test/httpServer/main.cpp
+++ test/httpServer/main.cpp
@@ -38,7 +38,7 @@
 #include "logger.h"
 using namespace dns;
 int main() { 
-    Httpd *httpserver = new Httpd("192.168.199.125",123);
+    Httpd *httpserver = new Httpd("192.168.199.125",2048);
     httpserver->start();
     return 0;
 }
